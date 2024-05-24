4f86f7e1e0f641a718bfd9b039656f9cab37a4e7
manjeet <manjeetpilania1@gmail.com>
2024-05-25 02:58:06 +0530

    first commit

---
+++ week 1/pyramid.py
@@ -0,0 +1,9 @@
+def pyramid(n):
+    for i in range(n):
+        for j in range(n - i - 1):
+            print(' ', end=' ')
+        for j in range(2 * i + 1):
+            print('*', end=' ')
+        print()
+
+pyramid(5)
